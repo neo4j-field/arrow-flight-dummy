f67531109d5ab16fd9e26015ccd186e3572151af
Dave Voutila <voutilad@gmail.com>
2022-05-27 12:35:03 -0400

    comments

--- app/openssl.sh
+++ app/openssl.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+# hardcode for now
 OUT_DIR="build/libs"
 CERTIFICATE="${OUT_DIR}/dummy.crt"
 PRIVATE_KEY="${OUT_DIR}/dummy.key"
 
+# We assume `gradle shadowJar` or the like has been run and created ${OUT_DIR}
 if [ ! -d "${OUT_DIR}" ]; then
   echo "${OUT_DIR} does not exist" >&2
   exit 1
 fi
 
+# Wipe any existing keys
 if [ -f "${CERTIFICATE}" ]; then unlink "${CERTIFICATE}"; fi
 if [ -f "${PRIVATE_KEY}" ]; then unlink "${PRIVATE_KEY}"; fi
 
+# Refresh our cert and key
 openssl req -x509 -newkey rsa:4096 \
   -keyout "${PRIVATE_KEY}" \
   -out "${CERTIFICATE}" \
