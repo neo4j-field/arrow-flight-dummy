5246b70280c781902e228426dfde6a0842f2c1d0
Dave Voutila <voutilad@gmail.com>
2022-05-27 12:14:30 -0400

    fix up args for using tls

--- app/src/main/java/com/neo4j/gds/flight/App.java
+++ app/src/main/java/com/neo4j/gds/flight/App.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
@@ -17,26 +18,25 @@ public class App implements AutoCloseable {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
     private final BufferAllocator rootAllocator;
-    private final Location location;
     private final FlightServer server;
+    private final Location location;
     private final Producer producer;
 
     public App(BufferAllocator rootAllocator, Location location) throws IOException {
         this(rootAllocator, location, null, null);
     }
 
-    public App(BufferAllocator rootAllocator, Location location, @Nullable Path cert, @Nullable Path privateKey) throws IOException {
-        this.rootAllocator = rootAllocator;
+    public App(BufferAllocator parentAllocator, Location location, @Nullable Path cert, @Nullable Path privateKey) throws IOException {
+        this.rootAllocator = parentAllocator.newChildAllocator("app", 0, Long.MAX_VALUE);
+        this.producer = new Producer(rootAllocator);
         this.location = location;
 
-        this.producer = new Producer(rootAllocator);
-
-        final FlightServer.Builder builder = FlightServer.builder(rootAllocator, location, producer);
+        final FlightServer.Builder builder = FlightServer.builder(this.rootAllocator, this.location, this.producer);
         // TODO: auth
 
         if (location.getUri().getScheme().equalsIgnoreCase(LocationSchemes.GRPC_TLS)) {
-            assert privateKey != null;
-            assert cert != null;
+            if (cert == null || privateKey == null)
+                throw new IllegalArgumentException("can't use grpc+tls if missing cert or private key");
             builder.useTls(cert.toFile(), privateKey.toFile());
         }
 
@@ -45,7 +45,7 @@ public class App implements AutoCloseable {
 
     public void start() throws IOException {
         server.start();
-        log.info("started FlightServer");
+        log.info("started FlightServer listing on {}", this.location);
     }
 
     public void shutdown(long timeout, TimeUnit unit) throws InterruptedException {
@@ -64,29 +64,37 @@ public class App implements AutoCloseable {
         server.awaitTermination();
     }
 
-    public static void main(String[] args) throws Exception {
-        log.info("Starting...");
+    public static void main(String ...args) throws Exception {
+        // This is silly...and case-sensitive
+        System.setProperty(DefaultAllocationManagerOption.ALLOCATION_MANAGER_TYPE_PROPERTY_NAME, "Netty");
 
-        final App app = new App(
-                new RootAllocator(RootAllocator.configBuilder()
-                        .maxAllocation(Long.MAX_VALUE)
-                        .allocationManagerFactory(NettyAllocationManager.FACTORY)
-                        .build()),
-                Location.forGrpcInsecure("localhost", 8491));
+        for (String arg : args)
+            log.debug("arg: {}", arg);
 
-        Runtime.getRuntime()
-                .addShutdownHook(new Thread(() -> {
-                    try {
-                        log.info("caught shutdown request");
-                        app.shutdown(1, TimeUnit.MINUTES);
-                    } catch (InterruptedException e) {
-                        log.error("timed out waiting for shutdown");
-                    } catch (Exception e) {
-                        log.error("failed to cleanly shutdown server", e);
-                    }
-                }, "shutdown-thread"));
+        var location = new Location(new URI(args.length > 0 ? args[0] : "grpc://0.0.0.0:8491"));
+        var config = RootAllocator
+                .configBuilder()
+                .maxAllocation(Long.MAX_VALUE)
+                .allocationManagerFactory(NettyAllocationManager.FACTORY);
+        var cert = (args.length > 1 ? Path.of(args[1]) : null);
+        var key = (args.length > 2 ? Path.of(args[2]) : null);
 
-        app.start();
-        app.await();
+        try (final RootAllocator allocator = new RootAllocator(config.build())) {
+            var app = new App(allocator, location, cert, key);
+            Runtime.getRuntime()
+                    .addShutdownHook(new Thread(() -> {
+                        try {
+                            log.info("caught shutdown request");
+                            app.shutdown(1, TimeUnit.MINUTES);
+                        } catch (InterruptedException e) {
+                            log.error("timed out waiting for shutdown");
+                        } catch (Exception e) {
+                            log.error("failed to cleanly shutdown server", e);
+                        }
+                    }, "shutdown-thread"));
+
+            app.start();
+            app.await();
+        }
     }
 }
